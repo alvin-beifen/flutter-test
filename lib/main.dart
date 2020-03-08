dcbc86b33c047e70b870db7f403187df45e97d44
alvin <xiuhongnie@gmail.com>
2020-03-08 20:15:47 +0800

    初始化dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,25 @@
 import 'package:flutter/material.dart';
-import 'keep_alive/keep_alive_demo.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '保持也没状态啊',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: KeepAliveDemo(),
-    );
-  }
-}
-
-
-class KeepAliveDemo extends StatefulWidget {
-  @override
-  _KeepAliveDemoState createState() => _KeepAliveDemoState();
-}
-
-class _KeepAliveDemoState extends State<KeepAliveDemo> with SingleTickerProviderStateMixin { // 混入？
-  TabController _controller;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _controller = TabController(length: 3, vsync: this); // 垂直。。。记住，这个属性在SingleTickerProviderStateMixin里面
-  }
-  @override
-  void dispose(){
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '保持页面状态',
+      title: '只是初始化的而已，看不到',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'dev分支',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.amberAccent
+            )
+          ),
         ),
-        bottom: TabBar(
-            controller: _controller,
-            tabs: [
-              Tab(icon: Icon(Icons.directions_car)),
-              Tab(icon: Icon(Icons.directions_transit)),
-              Tab(icon: Icon(Icons.directions_bike)),
-            ]
-          )
-      ),
-      body: TabBarView(
-        controller: _controller,
-        children: <Widget>[
-          MyHomePage(),
-          MyHomePage(),
-          MyHomePage(),
-        ],
+        body: Center(
+          child: Text('我是首页'),
+        )
       )
     );
   }
